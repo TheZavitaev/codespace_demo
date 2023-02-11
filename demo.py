c841792c22ece3efefc0e851513dc8cf4514442a
Oleg Zavitaev <ozavitaev@datafort.ru>
2023-02-11 21:00:02 +0800

    add demo file

--- demo.py
+++ demo.py
@@ -1,4 +1,11 @@
+from time import sleep
+
 import cowsay
 
 if __name__ == '__main__':
     cowsay.cow('Hello World')
+    sleep(2)
+    cowsay.fox(
+        'Теперь вы можете работать с проектом не только на домашнем компьютере, '
+        'но даже с телефона и/или планшета'
+    )
